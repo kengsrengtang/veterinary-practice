fac0ab9208102107ff412330a83db49f0a1e295a
Kengsreng Tang <kengsrengkh@gmail.com>
2014-02-08 11:09:20 -0800

    init new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VeterinaryPractice::Application.config.secret_key_base = 'd59c766e8e1d594e90a3ff7f6a3cbc325e6c85768913d16f939ffc2cb78f19f03c272d2ca991caef1a13acd74ecbce179229f82cbc57e8c56674c3151faae94e'
